a00a400acce7ca86a0292c9e4a8200928eb077c0
Reid McIlroy-Young <reidmcy@gmail.com>
2018-11-08 04:37:55 -0500

    Doing it my own way

--- main.py
+++ main.py
@@ -4,14 +4,35 @@ import time
 
 RTM_READ_DELAY = 1 # 1 second delay between reading from RTM
 
+class EventsHandler(object):
+    def __init__(self, client):
+        self.client = client
+
+    def send(self, message, channel):
+        self.client.api_call(
+            "chat.postMessage",
+            channel=channel,
+            text=message,
+        )
+
+    def handleEvent(self, message, channel):
+        if message == 'help':
+            self.send("Help requested", channel)
+        else:
+            self.send("Unkown command", channel)
+
+
+
+
+
 
 def main():
     slack_client = slack_handlers.setup()
+    Handler = EventsHandler(slack_client)
     while True:
-        command, channel = slack_handlers.parse_bot_commands(slack_client.rtm_read())
-        if command:
-            slack_handlers.handle_command(command, channel)
-        print(command, channel)
+        for event in slack_client.rtm_read():
+            if event['type'] == 'message':
+                handleEvents(event['text'])
         time.sleep(RTM_READ_DELAY)
 
 if __name__ == "__main__":
